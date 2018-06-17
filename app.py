e8bef11e2e790dd5b50e7aa5455cf7d6af809787
Chen Song <chensong5225@gmail.com>
2018-06-16 20:10:20 -0400

    stock app init

--- app.py
+++ app.py
@@ -1,14 +1,28 @@
 from flask import Flask, render_template, request, redirect
+from stock import stock_graph
+app_stock = Flask(__name__)
 
-app = Flask(__name__)
+app_stock.vars={}
 
-@app.route('/')
+@app_stock.route('/')
+def main():
+    return redirect('/index')
+
+@app_stock.route('/index', methods=['GET','POST'])
 def index():
-  return render_template('index.html')
+    if request.method == 'GET':
+        return render_template('index.html')
+    else:
+        app_stock.vars['stock_name'] = request.form['stock_name']
+        stock_graph(app_stock.vars['stock_name'])
+        return redirect('/stockgraph')
 
-@app.route('/about')
-def about():
-  return render_template('about.html')
+@app_stock.route('/stockgraph', methods=['GET','POST'])
+def stockgraph():
+    if request.method == 'GET':
+        return render_template('stockgraph.html')
+    else:
+        return redirect('/index')
 
-if __name__ == '__main__':
-  app.run(port=33507)
+if __name__ == "__main__":
+    app_stock.run(debug=True)
---
+++ stock.py
@@ -0,0 +1,37 @@
+import requests
+import datetime
+import simplejson as json
+import pandas as pd
+from bokeh.plotting import figure, show, output_file, save
+from bokeh.layouts import column
+
+def stock_graph(stock_name):
+
+    quandl_apikey = "W1_Zw8zAmSS8XEosenzw"
+    quandl_header = "https://www.quandl.com/api/v3/datasets/WIKI"
+
+    end_date = "2018-03-01"
+    start_date = "2017-03-01"
+
+    qurl='https://www.quandl.com/api/v3/datasets/WIKI/%s.json?column_index=4&start_date=%s&end_date=%s&api_key=W1_Zw8zAmSS8XEosenzw'%(stock_name, start_date, end_date)
+    r = requests.get(qurl)
+    if r.status_code != requests.codes.ok: return 0
+
+    data = r.json()['dataset']['data']
+
+    s1 = []
+    s2 = []
+    for price in data:
+        s1.insert(0,pd.Timestamp(price[0]))
+        s2.insert(0,price[1])
+
+    df = pd.DataFrame({'date':s1,'price':s2})
+    print(df)
+
+    graph_title = "2017-03-01 ~ 2018-03-01 Stock Price of " + stock_name
+
+    p = figure(x_axis_type="datetime", plot_width=800, plot_height=350, title=graph_title)
+    p.line('date', 'price', source=df)
+
+    save(obj=p, filename='templates/stockgraph.html')
+    return 1
